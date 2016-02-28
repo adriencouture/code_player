072bf8ac4d63a703f928be64de89a07bc5df2cb0
Adrien Couture <coutureadrien@gmail.com>
2016-02-27 21:01:05 -0500

    jsbin replica

---
+++ scripts.js
@@ -0,0 +1,26 @@
+var windowHeight = $(window).height();
+var menuBarHeight = $('#menuBar').height();
+var codeContainerHeight = windowHeight - menuBarHeight;
+
+$(".codeContainer").height(codeContainerHeight+"px"); // Specify the units of your height
+
+$(".toggle").click(function() {
+  $(this).toggleClass("selected");
+  var activeDiv = $(this).html();
+  $("#"+activeDiv+"Container").toggle();
+
+  var showingDivs = $(".codeContainer").filter(function(){
+    return($(this).css("display")!="none");
+  }).length;
+
+  var width = 100/showingDivs;
+  $(".codeContainer").width(width+"%");
+
+});
+
+$("#runButton").click(function() {
+  $("iframe").contents().find("html").html('<style>'+$("#cssCode").val()+'</style>'+$("#htmlCode").val());
+
+  document.getElementById("resultFrame").contentWindow.eval($("#jsCode").val());
+
+});
